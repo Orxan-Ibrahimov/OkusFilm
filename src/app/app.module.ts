b20133884ab889b7c23f835ab7b2bb7bd8fcdd2a
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2022-03-05 14:53:04 +0400

    firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { MoviesComponent } from './movies/movies.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MovieComponent } from './movies/movie/movie.component';
+import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    MoviesComponent,
+    NavbarComponent,
+    FooterComponent,
+    MovieComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule
