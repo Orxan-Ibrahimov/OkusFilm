d508f26400fcbb46bddcdc5c3d9116c2d670fe41
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2022-03-06 23:27:16 +0400

    AlertifyMethodsAdded

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieRepository } from '../models/movieRepository';
 import { Movies } from '../models/movıes';
+import { AlertifyService } from '../services/alertify.service';
 
 @Component({
   selector: 'app-movies',
@@ -15,14 +16,27 @@ export class MoviesComponent implements OnInit {
   movieRepository:MovieRepository;
   filteredText:string = "";
 
-
-  constructor() { 
+  constructor(private alert:AlertifyService) { 
     this.movieRepository = new MovieRepository();
     this.movies = this.movieRepository.getAllMovies();
     this.popularMovies = this.movieRepository.getPopularMovies();
   }
 
+  AddMovie($event:any,movie:Movies){
+    if($event.target.classList.contains('btn-primary')){
+      $event.target.classList.remove('btn-primary');
+      $event.target.classList.add('btn-danger');
+      $event.target.innerHTML = "Remove";
+      this.alert.success(`${movie.name} Added`);
+    }
+    else{
+      $event.target.classList.remove('btn-danger');
+      $event.target.classList.add('btn-primary');
+      $event.target.innerHTML = "Add";
+      this.alert.error(`${movie.name} Removed`);
 
+    }
+  }
 
   ngOnInit(): void {
   }
